e713fb02e931006a7635e48e143fe092e7e57ac2
minerva88 <46870332+minerva88@users.noreply.github.com>
2022-07-17 23:43:56 +0200

    Create
    Added form and functionality to create-page in admin section.

---
+++ js/create.js
@@ -0,0 +1,60 @@
+import { displayMessage } from "./common/displayMessage.js";
+import { getToken } from "./utils/storage.js";
+import { baseUrl } from "./constants/api.js";
+
+const form = document.querySelector('form');
+const postTitle = document.querySelector('#post-title');
+const excerpt = document.querySelector('#excerpt');
+const content = document.querySelector('#content');
+const message = document.querySelector('.message-container');
+
+form.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+    event.preventDefault();
+
+    message.innerHTML = '';
+
+    const titleValue = postTitle.value.trim();
+    const excerptValue = excerpt.value.trim();
+    const contentValue = content.value.trim();
+    const status = 'publish';
+
+    if(titleValue.length === 0 || excerptValue.length === 0 || contentValue.length === 0) {
+        displayMessage('warning', 'Please enter content to all inputs to post.', '.message-container');
+    }
+
+    addPost(titleValue, excerptValue, contentValue, status);
+}
+
+async function addPost(title, excerpt, content, status) {
+    const url = baseUrl + 'posts';
+
+    const data = JSON.stringify({ title: title, excerpt: excerpt, content: content, status: status });
+
+    const token = getToken();
+
+    const options = {
+        method: 'POST',
+        body: data,
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const json = await response.json();
+
+        console.log(json);
+
+        if(json.id) {
+            displayMessage('success', 'Your post has successfully been created!', '.message-container');
+            form.reset();
+        };
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
